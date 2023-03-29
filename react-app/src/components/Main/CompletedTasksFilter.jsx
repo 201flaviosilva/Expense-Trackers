6f359c394a80baa2326d7506f2793bc375e85d53
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2023-03-30 00:33:27 +0100

    fix(react): mobile responsiveness

--- react-app/src/components/Main/CompletedTasksFilter.jsx
+++ react-app/src/components/Main/CompletedTasksFilter.jsx
@@ -1,29 +1,43 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronUp } from "react-icons/go";
 import styles from "./style.module.scss";
 
+const ICON_SIZE = 16;
+
 export default function CompletedTasksFilter({ selectedCompleted, setSelectedCompleted }) {
+	const [isOpen, setIsOpen] = useState(false);
 	function onClick(value) {
 		setSelectedCompleted(value);
 	}
 
 	return (
 		<aside>
-			<RadioInput
-				value="All"
-				selectedCompleted={selectedCompleted}
-				onClick={onClick}
-			/>
+			<button
+				title="Filter by un/completed tasks"
+				className={styles.toggleBTN}
+				onClick={() => setIsOpen(!isOpen)}>
+				{isOpen ? <GoChevronDown size={ICON_SIZE} /> : <GoChevronUp size={ICON_SIZE} />}
+			</button>
 
-			<RadioInput
-				value="Completed"
-				selectedCompleted={selectedCompleted}
-				onClick={onClick}
-			/>
+			{isOpen && <div className={styles.container}>
+				<RadioInput
+					value="All"
+					selectedCompleted={selectedCompleted}
+					onClick={onClick}
+				/>
 
-			<RadioInput
-				value="Uncompleted"
-				selectedCompleted={selectedCompleted}
-				onClick={onClick}
-			/>
+				<RadioInput
+					value="Completed"
+					selectedCompleted={selectedCompleted}
+					onClick={onClick}
+				/>
+
+				<RadioInput
+					value="Uncompleted"
+					selectedCompleted={selectedCompleted}
+					onClick={onClick}
+				/>
+			</div>}
 		</aside>
 	)
 }
