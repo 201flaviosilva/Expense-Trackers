f7a35b98d33314d603d66eef5a05de3d59b22dcf
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2023-05-05 18:05:50 +0100

    feat(svelte): create a new task modal structure

--- svelte-bootstrap-app/src/lib/Main.svelte
+++ svelte-bootstrap-app/src/lib/Main.svelte
@@ -1,13 +1,29 @@
 <script>
-  import { Table } from "sveltestrap";
+  import Icon from "svelte-icons-pack/Icon.svelte";
+  import AiOutlinePlus from "svelte-icons-pack/ai/AiOutlinePlus";
+  import FaSolidBomb from "svelte-icons-pack/fa/FaSolidBomb";
+  import { Button, Table } from "sveltestrap";
+  import NewTransactionModal from "./NewTransactionModal.svelte";
   import Transaction from "./Transaction.svelte";
 
-  const transactionsList = [
-    { title: "Beep", amount: 0, date: new Date() },
-    { title: "Boop", amount: -10, date: new Date() },
-    { title: "Beop", amount: 10, date: new Date() },
-    { title: "asdsfsafasdasdasdasda", amount: -100, date: new Date() },
-  ];
+  // Get data from local storage
+  const LOCAL_NAME_SPACE = "Svelte-Bootstrap-App-Expense-Tracker";
+  const localTransactions =
+    JSON.parse(localStorage.getItem(LOCAL_NAME_SPACE)) || [];
+
+  // Type: {id: string, title: string, amount: number, date: Date}
+  const transactionsList = localTransactions;
+
+  // Update local storage
+  $: {
+    localStorage.setItem(LOCAL_NAME_SPACE, JSON.stringify(transactionsList));
+  }
+
+  // New Task Modal
+  let isOpen = false;
+  function toggle() {
+    isOpen = !isOpen;
+  }
 </script>
 
 <main>
@@ -21,6 +37,7 @@
         <th>Delete</th>
       </tr>
     </thead>
+
     <tbody>
       {#each transactionsList as transaction}
         <Transaction
@@ -31,10 +48,34 @@
       {/each}
     </tbody>
   </Table>
+
+  <!-- Action Buttons -->
+  <div class="action-buttons">
+    <Button
+      color="primary"
+      title="Add new Transaction"
+      on:click={() => (isOpen = true)}
+    >
+      <Icon src={AiOutlinePlus} color="white" size="1.5em" />
+    </Button>
+    <Button color="danger" title="Remove All">
+      <Icon src={FaSolidBomb} color="black" />
+    </Button>
+  </div>
 </main>
+
+<NewTransactionModal {isOpen} {toggle} />
 
 <style lang="scss">
   main {
     flex: 1;
+
+    .action-buttons {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+      padding: 4px;
+    }
   }
 </style>
---
+++ svelte-bootstrap-app/src/lib/NewTransactionModal.svelte
@@ -0,0 +1,33 @@
+<script>
+  import {
+    Button,
+    Modal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+  } from "sveltestrap";
+
+  export let isOpen = false;
+  export let toggle;
+
+  function create() {
+    toggle();
+  }
+</script>
+
+<Modal {isOpen} {toggle}>
+  <ModalHeader {toggle}>Create a new Transaction</ModalHeader>
+
+  <ModalBody>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.
+  </ModalBody>
+
+  <ModalFooter>
+    <Button color="success" on:click={create}>Create</Button>
+    <Button color="secondary" on:click={toggle}>Cancel</Button>
+  </ModalFooter>
+</Modal>
+
+<style lang="scss">
+</style>
